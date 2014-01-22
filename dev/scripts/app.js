67198c01f6bc6776f040a157f3b55c3f6a8adbee
Hektor Wallin <hektorw@gmail.com>
2014-01-22 23:18:02 +0100

    Added som threejs

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -8,21 +8,25 @@ define(['threejs'], function(Three) {
     
     this.scene = new Three.Scene();
     this.camera = new Three.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.camera.position.z = 5.0;
 
     this.renderer = new Three.CanvasRenderer();
     this.renderer.setSize( window.innerWidth, window.innerHeight );
-
 
     this.rootElement = document.getElementById('root');
     this.rootElement.appendChild(this.renderer.domElement);
 
     var geometry = new Three.CubeGeometry(1,1,1);
-    var material = new Three.MeshBasicMaterial({color: 0xFF4136});
+    var material = new Three.MeshPhongMaterial({color: 0xFF4136});
+    material.wireframe = true;
+    material.side = Three.DoubleSide;
     this.cube = new Three.Mesh(geometry, material);
-
     this.scene.add(this.cube);
 
-    this.camera.position.z = 5.0;
+
+    this.light = new Three.PointLight(0xffffff, 1, 100);
+    this.light.position.set(5, 5, 5);
+    this.scene.add(this.light);
 
     this._boundUpdate = this.update.bind(this);
     requestAnimationFrame(this._boundUpdate);
