b2f93fd01a2aae38e2f565a6a53586d3ead08a6d
Hektor Wallin <hektorw@gmail.com>
2014-02-06 23:00:17 +0100

    added more tests

--- test/scripts/tests.js
+++ test/scripts/tests.js
@@ -14,9 +14,7 @@ define([
 ) {
   
 
-  var assert = chai.assert,
-      expect = chai.expect,
-      should = chai.should;
+  var assert = chai.assert;
   
   function run() {
     console.log('Running tests');
@@ -32,10 +30,14 @@ define([
     // Format
     describe('StringUtil.format', function() {
       it('should format a string, replacing {n} with n:th passed in argument', function() {
-        assert(StringUtil.format("hello") === "hello");
-        assert(StringUtil.format("{0}", "hello") === "hello");
-        assert(StringUtil.format("When{0} you g{1}", "ever", "o") === "Whenever you go");
-        assert(StringUtil.format("when{0}, for{0}", "ever") === "whenever, forever");
+        assert.equal(StringUtil.format("hello"), "hello");
+        assert.equal(StringUtil.format("{0}", "hello"), "hello");
+        assert.equal(StringUtil.format("When{0} you g{1}", "ever", "o"), "Whenever you go");
+        assert.equal(StringUtil.format("when{0}, for{0}", "ever"), "whenever, forever");
+
+        assert.equal(StringUtil.format("when{0}, for{0}"), "when{0}, for{0}");
+
+        assert.notEqual(StringUtil.format("{0}", 'walla'), "{0}");
       });
     });
   }
@@ -44,10 +46,24 @@ define([
   function prefixTests() {
     Prefixer.prefixAll();
 
-    // rAF
-    describe("window.requestAnimationFrame", function() {
-      it('should be unprefixed and a working function', function() {
-        assert(window.requestAnimationFrame);
+    // prefixAll
+    describe("prefixer.prefixAll", function() {
+      it('should have masked prefixed functions', function() {
+        assert(window.requestAnimationFrame, 'requestAnimationFrame is prefixed');
+        assert(window.cancelAnimationFrame);
+        assert(window.performance.now);
+        assert(window.performance.now());
+      });
+    });
+
+    // prefixedCss
+    describe('prefix.prefixedCss', function() {
+      it('should prefix passed in property with vendors', function() {
+
+        assert(Prefixer.prefixedCss('transform', 'translateZ(0)') == (
+          '-webkit-transform:translateZ(0);-moz-transform:translateZ(0);-ms-transform:translateZ(0);-o-transform:translateZ(0);transform:translateZ(0);'
+        ));
+
       });
     });
   }
