bae5fc1a04a6ffbea193551d1269e3085304cfcb
Hektor Wallin <hektorw@gmail.com>
2014-02-13 19:34:43 +0100

    added color util

---
+++ dev/scripts/util/Color.js
@@ -0,0 +1,39 @@
+define([], function() {
+  return {
+    white: 0xffffff,
+    navy: 0x001f3f,
+    blue: 0x0074d9,
+    aqua: 0x7fdbff,
+    teal: 0x39cccc,
+    olive: 0x3d9970,
+    green: 0x2ecc40,
+    lime: 0x01ff70,
+    yellow: 0xffdc00,
+    orange: 0xff851b,
+    red: 0xff4136,
+    maroon: 0x85144b,
+    fuchsia: 0xf012be,
+    purple: 0xb10dc9,
+    white: 0xffffff,
+    silver: 0xdddddd,
+    gray: 0xaaaaaa,
+    black: 0x111111
+  };
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
